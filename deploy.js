184b1529c3e1f27f3aa76501914eacf4ebad860f
Jesse <45066435+jesse01987@users.noreply.github.com>
2023-06-28 20:47:24 +0200

    main — deployer

--- deploy.js
+++ deploy.js
@@ -1,6 +1,33 @@
 const pkg = require('./package.json');
+const { deploy } = require('sftp-sync-deploy');
+require('dotenv').config();
 
-require('deployer').upload({
-  ...pkg.uploadConfig,
-  forceUpload: true,
-});
+const upload = (config) => {
+  if (!process.env.FTP_PASS) {
+    throw Error('No password in process.env.FTP_PASS');
+  }
+
+  const options = {
+    host: config.host,
+    port: 22,
+    username: config.user || config.host, // Username is same as host
+    password: process.env.FTP_PASS,
+    localDir: config.localDir || '',
+    remoteDir: config.remoteDir || './www',
+  };
+  console.log('🚀', options);
+  deploy(options, {
+    dryRun: false,
+    exclude: ['node_modules', 'package.json', '.turbo'],
+    excludeMode: 'remove',
+    forceUpload: config.forceUpload ? Boolean(config.forceUpload) : true,
+  })
+    .then(() => {
+      console.log('Done');
+    })
+    .catch((err) => {
+      console.error('Error ', err);
+    });
+};
+
+upload(pkg.uploadConfig);
